a5f551f84e95d69d64e96f84f09df67da2c6f48f
VladimirKovaliev <kovaliev.v@list.ru>
2024-04-11 20:36:45 +0300

    added new path for home view

--- posts/urls.py
+++ posts/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import PostListCreateAPIView, PostRetrieveUpdateDestroyAPIView
-from .views import CustomPermissionDeniedHandler
+from .views import PostListCreateAPIView, PostRetrieveUpdateDestroyAPIView, HomeView
 from posts.apps import PostsConfig
 
 app_name = PostsConfig.name
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+
     path('posts/', PostListCreateAPIView.as_view(), name='post-list-create'),
     path('posts/<int:pk>/', PostRetrieveUpdateDestroyAPIView.as_view(), name='post-retrieve-update-destroy'),
 ]
 
-handler403 = CustomPermissionDeniedHandler
