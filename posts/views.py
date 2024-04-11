782070f4327ec9a837675faf07a83b7569a1ff5e
VladimirKovaliev <kovaliev.v@list.ru>
2024-04-11 16:47:11 +0300

    added new views

--- posts/views.py
+++ posts/views.py
@@ -1,2 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
 
+
+class PostListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [AllowAny]
+
+
+class PostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class CustomPermissionDeniedHandler:
+    def __init__(self, exc):
+        self.exc = exc
+
+    def __call__(self, *args, **kwargs):
+        return Response({'detail': 'У вас нет прав для выполнения этого действия'}, status=401)
