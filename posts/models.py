73003037020cde79fef905467f60c059e33b1b2b
VladimirKovaliev <kovaliev.v@list.ru>
2024-04-15 16:41:13 +0300

    new path to images

--- posts/models.py
+++ posts/models.py
@@ -2,14 +2,16 @@ from django.db import models
 
 from users.models import User
 
+NULLABLE = {'null': True, 'blank': True}
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Заголовок')
-    text = models.TextField(verbose_name='Текст')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name='Автор')
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Текст')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name='Автор', **NULLABLE)
     created_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_date = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
-    image = models.ImageField(upload_to='posts/', verbose_name='Изображение', blank=True, null=True)
+    image = models.ImageField(upload_to='posts_images/', verbose_name='Изображение', **NULLABLE)
 
     def __str__(self):
         return self.title
