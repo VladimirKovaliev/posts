16d1dcebd0725e1f717b07298da0b3e3aae2be86
VladimirKovaliev <kovaliev.v@list.ru>
2024-04-16 12:46:58 +0300

    добавил stripe

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from users import views
 from users.apps import UsersConfig
 from users.views import UserLogoutView, UserLoginView, RegisterView, UserUpdateView
 
@@ -13,5 +14,8 @@ urlpatterns = [
     path('verify_message/', TemplateView.as_view(template_name='users/verify_message.html'), name='verify_message'),
     path('success_verify/', TemplateView.as_view(template_name='users/success_verify.html'), name='success_verify'),
     path('invalid_verify/', TemplateView.as_view(template_name='users/invalid_verify.html'), name='invalid_verify'),
-    path('edit/<int:pk>', UserUpdateView.as_view(), name='edit')
+    path('edit/<int:pk>', UserUpdateView.as_view(), name='edit'),
+
+    path('subscription-plans/', views.subscription_plans, name='subscription_plans'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 from django.urls import reverse
 from django.views.generic import CreateView, UpdateView
@@ -8,7 +8,9 @@ from users.forms import RegisterForm
 from users.models import User
 from django.views import View
 from django.contrib.auth import logout
-
+from django.conf import settings
+from django.shortcuts import render, redirect
+import stripe
 
 
 class UserLoginView(LoginView):
@@ -44,3 +46,50 @@ class UserUpdateView(PermissionRequiredMixin, UpdateView):
 
     def get_success_url(self):
         return reverse('users:list_view')
+
+
+# def subscription_plans(request):
+#     plans = [
+#         {'name': 'Basic', 'price': '$10', 'description': 'Базовая подписка на 1 месяц', 'duration': '1 месяц'},
+#         {'name': 'Standard', 'price': '$20', 'description': 'Стандартная подписка на 3 месяца', 'duration': '3 месяца'},
+#         {'name': 'Premium', 'price': '$50', 'description': 'Премиум подписка на 1 год', 'duration': '1 год'},
+#     ]
+#     return render(request, 'users/subscription_plans.html', {'plans': plans})
+
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+def subscription_plans(request):
+    if request.method == 'POST':
+        # Получаем данные о выбранном плане подписки
+        plan_name = request.POST.get('plan_name')
+
+        # Создаем платеж через Stripe
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': plan_name,
+                    },
+                    'unit_amount': 1000,  # Цена плана в центах
+                },
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url=request.build_absolute_uri('/users/success/'),
+            cancel_url=request.build_absolute_uri('/users/subscription_plans/'),
+        )
+
+        # Перенаправляем пользователя на страницу оплаты Stripe
+        return redirect(session.url)
+
+    # Если запрос GET, просто отображаем страницу с планами подписок
+    plans = [
+        {'name': 'Basic', 'price': '$10', 'description': 'Базовая подписка на 1 месяц', 'duration': '1 месяц'},
+        {'name': 'Standard', 'price': '$20', 'description': 'Стандартная подписка на 3 месяца', 'duration': '3 месяца'},
+        {'name': 'Premium', 'price': '$50', 'description': 'Премиум подписка на 1 год', 'duration': '1 год'},
+    ]
+    return render(request, 'users/subscription_plans.html', {'plans': plans})
